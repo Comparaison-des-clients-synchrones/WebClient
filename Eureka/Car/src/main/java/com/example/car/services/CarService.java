517606bd8dcaf872327b492d14e0572ff2388c39
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2025-01-14 01:31:33 +0100

    eureka + consul

---
+++ Eureka/Car/src/main/java/com/example/car/services/CarService.java
@@ -0,0 +1,37 @@
+package com.example.car.services;
+
+import com.example.car.entities.Car;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private final WebClient.Builder webClientBuilder;
+
+    public CarService(WebClient.Builder webClientBuilder){
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    public List<Car> findAll(){
+        return webClientBuilder.build()
+                .get()
+                .uri("http://SERVICE-CLIENT/api/client")
+                .retrieve()
+                .bodyToFlux(Car.class)
+                .collectList()
+                .block();
+    }
+
+    public Car findById(Long id){
+        return webClientBuilder.build()
+                .get()
+                .uri("http://SERVICE-CLIENT/api/client/{id}")
+                .retrieve()
+                .bodyToMono(Car.class)
+                .block();
+    };
+
+}
